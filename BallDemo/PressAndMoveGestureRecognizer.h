cd1ad7cade169b0e281d6d855897418d922dca16
Marc <mjcuva@gmail.com>
2013-05-19 16:30:42 -0500

    Added gesture recognizer to allow for more fine tuned movement of the ball.

---
+++ BallDemo/PressAndMoveGestureRecognizer.h
@@ -0,0 +1,13 @@
+//
+//  PressAndMoveGestureRecognizer.h
+//  Balls of Fun
+//
+//  Created by Marc Cuva on 5/18/13.
+//  Copyright (c) 2013 Marc Cuva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PressAndMoveGestureRecognizer : UIGestureRecognizer
+@property (nonatomic) CGPoint translation;
+@end
---
+++ BallDemo/PressAndMoveGestureRecognizer.m
@@ -0,0 +1,55 @@
+//
+//  PressAndMoveGestureRecognizer.m
+//  Balls of Fun
+//
+//  Created by Marc Cuva on 5/18/13.
+//  Copyright (c) 2013 Marc Cuva. All rights reserved.
+//
+
+#import "PressAndMoveGestureRecognizer.h"
+#import <UIKit/UIGestureRecognizerSubclass.h>
+
+@interface PressAndMoveGestureRecognizer()
+@property (nonatomic) CGPoint previous;
+@property (nonatomic) CGPoint initial;
+@property (nonatomic) NSDate *start;
+@end
+
+@implementation PressAndMoveGestureRecognizer
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    [super touchesBegan:touches withEvent:event];
+    if([touches count] != 1){
+        self.state = UIGestureRecognizerStateFailed;
+    }else{
+        self.previous = self.initial = [touches.anyObject locationInView:[self.view window]];
+        self.start = [NSDate date];
+    }
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event{
+    [super touchesMoved:touches withEvent:event];
+    UIWindow *win = [self.view window];
+    CGPoint point = [touches.anyObject locationInView:win];
+    self.translation = CGPointMake(point.x - self.previous.x, point.y - self.previous.y);
+    self.previous = point;
+    self.state = UIGestureRecognizerStateChanged;
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event{
+    [super touchesEnded:touches withEvent:event];
+    
+}
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event{
+    [super touchesCancelled:touches withEvent:event];
+    self.translation = CGPointZero;
+    self.state = UIGestureRecognizerStateCancelled;
+}
+
+- (void)reset{
+    [super reset];
+    self.previous = CGPointZero;
+    self.translation = CGPointZero;
+}
+
+@end
